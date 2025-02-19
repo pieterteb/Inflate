c8ffa76c9300f6b4b09e0e4e6872a9c36183e4c7
Pieter te Brake <pieterteb@gmail.com>
2025-02-19 21:50:36 +0100

    Improved huffman table generating function and added function that returns value corresponding to the next huffman code.

--- include/huffman.h
+++ include/huffman.h
@@ -2,11 +2,27 @@
 #define HUFFMAN_H
 
 #include <stddef.h>
-#include <stdint.h>
 
 
 
-unsigned int* huffmanTable(size_t* code_lengths, size_t code_count, size_t* table_size, size_t* max_code_length);
+/**
+ * @brief Generates a deflate compliant Huffman table given an array of code lengths. The huffman table elements contain the value in the first 16 bits and the length of the code in the next 16 bits.
+ * 
+ * @param code_lengths Array of code lengths.
+ * @param code_count Number of codes.
+ * @return unsigned* 
+ */
+unsigned int* huffmanTable(size_t* code_lengths, size_t code_count);
+
+/**
+ * @brief Consumes and returns the value corresponding to the next code in @a bit_reader.
+ * 
+ * @param bit_reader Contains compressed data.
+ * @param table Huffman table.
+ * @param max_code_length Maximum code length encountered in @a table.
+ * @return unsigned int 
+ */
+unsigned int getValue(BitReader* bit_reader, unsigned int* table, size_t max_code_length);
 
 
 
--- src/huffman.c
+++ src/huffman.c
@@ -3,11 +3,12 @@
 #include <string.h>
 
 #include "huffman.h"
+#include "bit_reader.h"
 #include "inflate.h"
 
 
 
-unsigned int* huffmanTable(size_t* code_lengths, size_t code_count, size_t* table_size, size_t* max_code_length) {
+unsigned int* huffmanTable(size_t* code_lengths, size_t code_count) {
     size_t code_lenght_frequency[INFLATE_MAX_CODE_LENGTH + 1] = { 0 };
     for (size_t i = 0; i < code_count; ++i) {
         ++code_lenght_frequency[code_lengths[i]];
@@ -16,36 +17,60 @@ unsigned int* huffmanTable(size_t* code_lengths, size_t code_count, size_t* tabl
     /* Determine lowest possible code for each code length. */
     unsigned int code = 0;
     unsigned int next_code[INFLATE_MAX_CODE_LENGTH + 1] = { 0 };
+    size_t max_code_length = 0;
     for (size_t i = 1; i <= INFLATE_MAX_CODE_LENGTH; ++i) {
         code = (code + code_lenght_frequency[i - 1]) << 1;
         next_code[i] = code;
         if (code_lenght_frequency[i]) {
-            *max_code_length = i;
+            max_code_length = i;
         }
     }
 
-    *table_size = next_code[*max_code_length] + code_lenght_frequency[*max_code_length];
-    unsigned int* table = malloc(*table_size * sizeof(*table));
-    memset(table, INFLATE_UNUSED_CODE, *table_size * sizeof(*table));
+    size_t table_size = 1U << max_code_length;
+    unsigned int* table = malloc(table_size * sizeof(*table));
+    memset(table, INFLATE_UNUSED_CODE, table_size * sizeof(*table));
 
     /* Map codes to values. */
     size_t current_length = 0;
     for (size_t i = 0; i < code_count; ++i) {
         current_length = code_lengths[i];
         if (current_length) {
-            table[next_code[current_length] << *max_code_length - current_length] = (unsigned int)i;
+            table[next_code[current_length] << max_code_length - current_length] = (unsigned int)current_length << 16 | (unsigned int)i;
             ++next_code[current_length];
         }
     }
 
-    unsigned int current_code = table[*table_size - 1];
-    for (size_t i = 0; i < *table_size; ++i) {
+    unsigned int current_code = table[0] & 0xFFFF;
+    current_length = table[0] >> 16;
+    for (size_t i = 0; i < table_size; ++i) {
         if (table[i] == INFLATE_UNUSED_CODE) {
-            table[i] = current_code;
+            table[i] = current_length << 16 | current_code;
         } else {
-            current_code = table[i];
+            current_code = table[i] & 0xFFFF;
+            current_length = table[i] >> 16;
         }
     }
 
     return table;
+}
+
+unsigned int getValue(BitReader* bit_reader, unsigned int* table, size_t max_code_length) {
+    unsigned int bit = peekBits(bit_reader, 1);
+    if (bit == (unsigned int)-1) {
+        return (unsigned int)-1;
+    }
+
+    unsigned int code = 0;
+    for (size_t i = 0; i < max_code_length;) {
+        code = code << 1 | bit;
+    
+        ++i;
+        bit = peekBits(bit_reader, 1);
+        if (bit == (unsigned int)-1) {
+            code <<= max_code_length - i;
+        }
+    }
+    getBits(bit_reader, table[code] >> 16);
+
+    return table[code] & 0xFFFF;
 }
